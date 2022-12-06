881cdfa0723baaa164269025baa33ac041f8e0a2
Luca Del Puppo <luca@delpuppo.net>
2022-12-06 19:57:11 +0100

    locators, assertions and many more

--- e2e/tit-tac-toe.spec.ts
+++ e2e/tit-tac-toe.spec.ts
@@ -1,7 +1,40 @@
 import { expect, test } from "@playwright/test";
 
-test("show tic tac toe page", async ({ page }) => {
-  await page.goto("/");
+test.describe("On View", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
 
-  await expect(page).toHaveTitle("Tic Tac Toe");
+  test("show tic tac toe page", async ({ page }) => {
+    await expect(page).toHaveTitle("Tic Tac Toe");
+  });
+
+  test('the first player must be the "X" player', async ({ page }) => {
+    const playerParagraph = await page.getByRole("paragraph");
+
+    await expect(playerParagraph).toContainText("X");
+  });
+});
+
+test.describe("Users behaviours", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
+
+  test('the first player select the top-left square and then the next player is the "O"', async ({
+    page,
+  }) => {
+    const squares = await page.getByRole("button");
+
+    const topLeftSquare = squares.first();
+    await topLeftSquare.click();
+    const topLeftSquareImg = await topLeftSquare.getByRole("img");
+
+    await expect(topLeftSquareImg).toHaveAttribute("title", "X");
+    await expect(topLeftSquareImg).toHaveAttribute("src", /x.png/i);
+
+    const playerParagraph = await page.getByRole("paragraph");
+
+    await expect(playerParagraph).toContainText("O");
+  });
 });
